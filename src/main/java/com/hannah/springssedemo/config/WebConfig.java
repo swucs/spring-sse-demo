ec3451b016fa88bf6afe81b98cd797e6f4b31abd
박한나/오비고 <swucs7@gmail.com>
2024-10-04 14:37:56 +0900

    수정

--- src/main/java/com/hannah/springssedemo/config/WebConfig.java
+++ src/main/java/com/hannah/springssedemo/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.hannah.springssedemo.config;
 
 import com.hannah.springssedemo.interceptor.JwtTokenInterceptor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,17 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final JwtTokenInterceptor jwtTokenInterceptor;
-
-
-//    @Value("${cors.allowed-headers}")
-//    private String allowedHeaders;
-//
-//    @Value("${cors.allowed-methods}")
-//    private String allowedMethods;
-//
-//    @Value("${cors.allowed-origin-patterns}")
-//    private String allowedOriginPatterns;
-
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
